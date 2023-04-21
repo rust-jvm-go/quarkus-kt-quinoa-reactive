2a250ac236156d2c57f5b92d3567312ebb7f761b
rust-jvm-go <rustxjvmxgo@gmail.com>
2023-04-22 02:43:50 +0800

    Routing

---
+++ src/main/webui/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtWelcome />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
